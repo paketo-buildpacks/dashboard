db5c09dec994d0215be95c3243b053c4c330e3d4
Ryan Moran <rmoran@vmware.com>
2020-10-08 15:35:58 -0700

    Adds production build directory

---
+++ precache-manifest.328b55565b13b8e0a51411c37a7e4ec7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5777ccf4564ad29fae29c27c5ae877",
+    "url": "/paketo-dashboard/index.html"
+  },
+  {
+    "revision": "784898e319684efb6d1d",
+    "url": "/paketo-dashboard/static/css/main.fdaeb553.chunk.css"
+  },
+  {
+    "revision": "18fe26a9b6472ce6a0df",
+    "url": "/paketo-dashboard/static/js/2.c0d99af6.chunk.js"
+  },
+  {
+    "revision": "05a8536829d95307bf7b8ace31dec2ef",
+    "url": "/paketo-dashboard/static/js/2.c0d99af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784898e319684efb6d1d",
+    "url": "/paketo-dashboard/static/js/main.a0de786c.chunk.js"
+  },
+  {
+    "revision": "eb4155b0afbe9a210c00",
+    "url": "/paketo-dashboard/static/js/runtime-main.2d71cb59.js"
+  }
+]);

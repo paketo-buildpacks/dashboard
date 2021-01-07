1d76eb51f360955cd6bf5152e2b62828343cb74f
Ryan Moran <rmoran@vmware.com>
2021-01-06 18:29:06 -0800

    Build production app

---
+++ precache-manifest.62a16142a015eeb772dd0efafe2b0472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ae80cd5cb259dc99ef1c59e5aa66b5",
+    "url": "/paketo-dashboard/index.html"
+  },
+  {
+    "revision": "c3b73081dc0fb980d52e",
+    "url": "/paketo-dashboard/static/css/main.bf3490c0.chunk.css"
+  },
+  {
+    "revision": "89e4abad4f95e11ef88b",
+    "url": "/paketo-dashboard/static/js/2.0eadfdce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/paketo-dashboard/static/js/2.0eadfdce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3b73081dc0fb980d52e",
+    "url": "/paketo-dashboard/static/js/main.8b75b590.chunk.js"
+  },
+  {
+    "revision": "eb4155b0afbe9a210c00",
+    "url": "/paketo-dashboard/static/js/runtime-main.2d71cb59.js"
+  }
+]);
